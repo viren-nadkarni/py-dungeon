bbc554e99ece603ca2c86b5db1c36f8a5ab23700
viren nadkarni <viren@fake.server>
2013-09-19 20:27:58 +0530

    visual borders

--- levels/map.py
+++ levels/map.py
@@ -1,10 +1,24 @@
+import sys
 class Map:
     def __init__(self):
         self.level = 0
         self.map = []
         
     def display(self):
+        a=0
+        while a<len(self.map[0]):
+            sys.stdout.write('--')
+            a=a+1
+        sys.stdout.write("-\n")  
+    
         for i in xrange( len( self.map ) ):
+            sys.stdout.write('|')
             for j in xrange( len( self.map[i] ) ):
-                print self.map[i][j],
-            print ' '
+                sys.stdout.write(self.map[i][j]+'|')
+            sys.stdout.write("\n")
+            a=0
+            
+            while a<len(self.map[0]):
+                sys.stdout.write('--')
+                a=a+1
+            sys.stdout.write("-\n")
