41789fe95c8374488c393292a6c7b4ef66a7bcac
viren nadkarni <viren@outlook.com>
2013-09-19 21:34:36 +0530

    fix bugs

--- player.py
+++ player.py
@@ -1,1 +1,9 @@
-# player.py
+# file: player.py
+# the method turn() will be run until the end of the dungeon is reached or an
+# error is encountered
+
+#if __name__ == '__main__':
+def turn(hero):
+    # your code here
+    hero.move('right')
+
--- units/hero.py
+++ units/hero.py
@@ -1,7 +1,8 @@
 import base_unit
-
+import sys
 class Hero(base_unit.BaseUnit):
-    def __init__(self):
+    def __init__(self, lvl):
+        self.lvl = lvl
         self.max_health = 100
         self.attack_power = 25
         self.health = 100
@@ -9,38 +10,52 @@ class Hero(base_unit.BaseUnit):
     def rest():
         if health < max_health and health + max_health/10 < max_health:
             health += max_health/10
-    
-    def position(self,lvl):
+
+    def heroPosition(self):
         x=0
         y=0
-        for i in lvl.map:
+        for i in self.lvl.map:
             if 'H' in i:
                 y=i.index('H')
                 break
             else:
                 x+=1
         return [x,y]
-            
-    def feel(self,dir,lvl):
-            pstn=self.position(lvl)
-            if(dir=='left'):
-                return lvl.map[pstn[0]][pstn[1]-1]
-            elif dir=='right':
-                return lvl.map[pstn[0]][pstn[1]+1]
-            elif dir=='up':
-                return lvl.map[pstn[0]-1][pstn[1]]
-            elif dir=='down':
-                return lvl.map[pstn[0]+1][pstn[1]]
-              
-    def walk(self,dir,lvl):
-            pstn=self.position(lvl)
-            if self.feel(dir,lvl) ==' ':
-                lvl.map[pstn[0]][pstn[1]]=' '
+
+    def feel(self,dir):
+            position=self.heroPosition()
+            try:
                 if(dir=='left'):
-                    lvl.map[pstn[0]][pstn[1]-1]='H'
+                    return self.lvl.map[position[0]][position[1]-1]
                 elif dir=='right':
-                    lvl.map[pstn[0]][pstn[1]+1]='H'
+                    return self.lvl.map[position[0]][position[1]+1]
                 elif dir=='up':
-                    lvl.map[pstn[0]-1][pstn[1]]='H'
+                    return self.lvl.map[position[0]-1][position[1]]
                 elif dir=='down':
-                    lvl.map[pstn[0]+1][pstn[1]]='H'
+                    return self.lvl.map[position[0]+1][position[1]]
+            except IndexError:
+                print "There is no escape!"
+                sys.exit()
+
+    def move(self,dir):
+            position=self.heroPosition()
+            try:
+                if self.feel(dir) ==' ':
+                    self.lvl.map[position[0]][position[1]]=' '
+                    if(dir=='left'):
+                        self.lvl.map[position[0]][position[1]-1]='H'
+                    elif dir=='right':
+                        self.lvl.map[position[0]][position[1]+1]='H'
+                    elif dir=='up':
+                        self.lvl.map[position[0]][1,position[1]]='H'
+                    elif dir=='down':
+                        self.lvl.map[position[0]][1,position[1]]='H'
+            except IndexError:
+                print "There is no escape!"
+                sys.exit()
+    def rope(self):
+        position=self.heroPosition()
+            if position[0]<(len(self.lvl.map[0]-1) and position[1]<len(self.lvl.map[1]-1):
+                    self.lvl.map[position[0]][position[1]+1] or 
+            print "LEVEL COMPLETE!"
+            sys.exit()
