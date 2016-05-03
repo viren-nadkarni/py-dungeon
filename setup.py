b24ad825587a2db9546f39ecaed7519d2ce5bb86
Viren Nadkarni <viren@outlook.com>
2016-05-03 06:02:42 -0500

    setuptools fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import os
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(name='pydungeon',
       version='0.1',
@@ -9,7 +12,9 @@ setup(name='pydungeon',
       author_email='viren@outlook.com',
       install_requires=INSTALL_REQUIRES,
       packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
       entry_points={
-          'gui_scripts': ['pydungeon = pydungeon.__main__:main']
+          'console_scripts': ['pydungeon = pydungeon.__main__:main']
       }
 )
